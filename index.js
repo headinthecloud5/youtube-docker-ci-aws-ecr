0d5205db741b0944b4faf8021f306ef9dad83821
Betsy Cates <betsycates@betsycates-macbook-air.tailcb7ae.ts.net>
2025-05-14 10:52:02 -0400

    ECR push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send(`
     <div style="background: linear-gradient(135deg, #FFDAB9, #FFA07A, #FF7F50); height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center; color: white; font-size: 36px; text-align: center; font-family: 'Roboto', sans-serif;">
-      <div style="margin-bottom: 20px;">Hello, welcome to <span style="font-weight: bold;">Headintheclouds!</span> :)</div>
+      <div style="margin-bottom: 20px;">Hello, welcome to <span style="font-weight: bold;">this APP</span> :)</div>
       <p style="font-size: 16px; font-weight: normal;">This is a Docker CI/CD pipeline tutorial using GitHub Actions to build and push images to Amazon ECR.</p>
     </div>
   `);
